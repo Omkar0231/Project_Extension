02cd949191ca9d91934d603bb4ed3bbd5cf23c7e
Ajay <email.ajaysinghroha@gmail.com>
2019-10-11 18:55:14 +0530

    added my deals content

--- homePage/script.js
+++ homePage/script.js
@@ -1,14 +1,26 @@
+document.cookie = 'same-site-cookie=foo; SameSite=Lax';
+document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
 $(document).ready(function () {
-   
-    $('#dealsTab').on('click', function () {
-        $('#myKidContent').css('z-index', '5');
-        $('#myDealsContent').css('z-index', '10');
 
+    var tabs = $('.tabs');
+    var selector = $('.tabs').find('a').length;
+    var selector = $(".tabs").find(".selector");
+    var activeItem = tabs.find('.active');
+    var activeWidth = activeItem.innerWidth();
+    $(".selector").css({
+        "left": activeItem.position.left + "px",
+        "width": activeWidth + "px"
     });
-    
-    $('#kidsTab').on('click', function () {
-        $('#myDealsContent').css('z-index', '5');
-        $('#myKidContent').css('z-index', '10');
 
+    $(".tabs").on("click", "a", function (e) {
+        e.preventDefault();
+        $('.tabs a').removeClass("active");
+        $(this).addClass('active');
+        var activeWidth = $(this).innerWidth();
+        var itemPos = $(this).position();
+        $(".selector").css({
+            "left": itemPos.left + "px",
+            "width": activeWidth + "px"
+        });
     });
-})
+});
