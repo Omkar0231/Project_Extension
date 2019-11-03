eb38e341203fbecd040aadfe3ada473adbd3fa6d
Omkar0231 <omkar.thouta0231@gmail.com>
2019-11-03 10:17:36 +0530

    Google Signin implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,3 +125,11 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
+
+/* Profile Photo DropDown */
+
+document.querySelector('.mini-photo-wrapper').addEventListener('click', function() {
+    document.querySelector('.menu-container').classList.toggle('active');
+  });
+
+/* Profile Photo DropDown */
