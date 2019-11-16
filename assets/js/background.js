9440f53fb963e25d05e505e7b322eb3c04c64a6f
ajay <email.ajaysinghroha@gmail.com>
2019-11-16 10:19:11 +0530

    chrome.oninstalled

--- assets/js/background.js
+++ assets/js/background.js
@@ -2,6 +2,9 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ "value": "true" }, function () {
     });
 });
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.tabs.create({url: '../homePage/index.html'});
+  });
 chrome.runtime.onSuspend.addListener(function () {
     console.log("Unloading.");
     chrome.browserAction.setBadgeText({
